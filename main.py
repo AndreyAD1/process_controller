d581ca5a722639a7293ec682a7760f353c47fbed
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2024-07-21 15:34:41 +0300

    run in a separate process and try to fix a lock usage

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import time
 from threading import Thread
 
+task_counter_lock = mp.Lock()
+runner_counter_lock = mp.Lock()
+
 
 def get_test_task(timeout):
     print(f"start to sleep {timeout} s : {os.getpid()}")
@@ -23,53 +26,65 @@ class ProcessController:
         self.runner_counter = 0
         self.max_timeout = None
         self.max_process_number = None
-        self._result = None
-        self._process_pool = None
+        self._work_process = None
+
+    @staticmethod
+    def _init_pool_process(task_lock, runner_lock):
+        global task_counter_lock
+        global runner_counter_lock
+        task_counter_lock = task_lock
+        runner_counter_lock = runner_lock
 
     def set_max_proc(self, max_process):
         self.max_process_number = max_process
 
     def _complete_runner(self, _):
-        self.runner_counter_lock.acquire()
+        runner_counter_lock.acquire()
         self.runner_counter -= 1
-        self.runner_counter_lock.release()
+        runner_counter_lock.release()
 
     def _error_callback(self, error):
         print("ERROR: ", error)
         self._complete_runner(error)
 
     def runner(self, func, *args):
-        self.task_counter_lock.acquire()
-        self.runner_counter_lock.acquire()
+        task_counter_lock.acquire()
+        runner_counter_lock.acquire()
         try:
             task_thread = Thread(target=func, args=args)
             task_thread.start()
             self.task_counter -= 1
             self.runner_counter += 1
         finally:
-            self.task_counter_lock.release()
-            self.runner_counter_lock.release()
+            task_counter_lock.release()
+            runner_counter_lock.release()
 
         task_thread.join(self.max_timeout)
+
+    def _start(self, tasks):
+        with mp.Pool(
+            processes=self.max_process_number,
+            initializer=self._init_pool_process,
+            initargs=(self.task_counter_lock, self.runner_counter_lock),
+        ) as process_pool:
+            result = process_pool.starmap_async(
+                self.runner,
+                tasks,
+                callback=self._complete_runner,
+                error_callback=self._error_callback
+            )
+            result.wait()
 
     def start(self, tasks, max_timeout):
         self.max_timeout = max_timeout
         self.task_counter = len(tasks)
         updated_tasks = [(t[0], *t[1]) for t in tasks]
-        process_pool = mp.Pool(processes=self.max_process_number)
-        result = process_pool.starmap_async(
-            self.runner,
-            updated_tasks,
-            callback=self._complete_runner,
-            error_callback=self._error_callback
-        )
-        self._process_pool = process_pool
-        self._result = result
+        p = mp.Process(target=self._start, args=(updated_tasks,))
+        self._work_process = p
+        p.start()
 
     def wait(self):
-        self._result.wait()
-        self._process_pool.close()
-        self._process_pool.join()
+        self._work_process.join()
 
     def alive_count(self):
         return self.runner_counter
