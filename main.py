3b6a0bf7a1b5142aee9a43c9a4a4b0142ae489a5
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2024-07-21 14:09:34 +0300

    add a class draft. Additional logic to properly close a pool is needed

--- main.py
+++ main.py
@@ -10,54 +10,103 @@ def get_test_task(timeout):
     print(f"wake up after {timeout} s : {os.getpid()}")
 
 
-task_counter_lock = mp.Lock()
-runner_counter_lock = mp.Lock()
-MAX_TIMEOUT = 5
-tasks = [
-    (get_test_task, 5),
-    (get_test_task, 2,),
-    (get_test_task, 1,),
-    (get_test_task, 3,),
-    (get_test_task, 0.5,)
-]
-task_counter = len(tasks)
-runner_counter = 0
+def another_task(a, b, c):
+    time.sleep(2)
+    print("calculation result: ", a * b - c)
 
 
-def complete_runner(_):
-    global runner_counter
-    runner_counter_lock.acquire()
-    runner_counter -= 1
-    runner_counter_lock.release()
+# task_counter_lock = mp.Lock()
+# runner_counter_lock = mp.Lock()
+# MAX_TIMEOUT = 4
+# tasks = [
+#     (get_test_task, 5),
+#     (get_test_task, 2,),
+#     (get_test_task, 1,),
+#     (get_test_task, 3,),
+#     (get_test_task, 0.5,)
+# ]
+# task_counter = len(tasks)
+# runner_counter = 0
 
 
-def runner(func, *args):
-    global task_counter
-    global runner_counter
+class ProcessController:
+    def __init__(self):
+        self.task_counter_lock = mp.Lock()
+        self.runner_counter_lock = mp.Lock()
+        self.task_counter = 0
+        self.runner_counter = 0
+        self.max_timeout = None
+        self.max_process_number = None
+        self._result = None
+        self._process_pool = None
 
-    task_counter_lock.acquire()
-    runner_counter_lock.acquire()
-    try:
-        task_thread = Thread(target=func, args=args)
-        task_thread.start()
-        task_counter -= 1
-        runner_counter += 1
-    finally:
-        task_counter_lock.release()
-        runner_counter_lock.release()
+    def set_max_proc(self, max_process):
+        self.max_process_number = max_process
 
-    task_thread.join(MAX_TIMEOUT)
+    def _complete_runner(self):
+        self.runner_counter_lock.acquire()
+        self.runner_counter -= 1
+        self.runner_counter_lock.release()
+
+    def runner(self, func, *args):
+        self.task_counter_lock.acquire()
+        self.runner_counter_lock.acquire()
+        try:
+            task_thread = Thread(target=func, args=args)
+            task_thread.start()
+            self.task_counter -= 1
+            self.runner_counter += 1
+        finally:
+            self.task_counter_lock.release()
+            self.runner_counter_lock.release()
+
+        task_thread.join(self.max_timeout)
+
+    def start(self, tasks, max_timeout):
+        self.max_timeout = max_timeout
+        updated_tasks = ...
+        process_pool = mp.Pool(processes=self.max_process_number)
+        result = process_pool.starmap_async(
+            self.runner,
+            updated_tasks,
+            callback=self._complete_runner,
+            error_callback=self._complete_runner
+        )
+        self._process_pool = process_pool
+        self._result = result
+
+    def wait(self):
+        self._result.join()
+        self._process_pool.close()
+        self._process_pool.join()
+
+    def alive_count(self):
+        return self.runner_counter
+
+    def wait_count(self):
+        return self.task_counter
 
 
 def main():
-    with mp.Pool(processes=3) as process_pool:
-        result = process_pool.starmap_async(
-            runner,
-            tasks,
-            callback=complete_runner,
-            error_callback=complete_runner
-        )
-        result.get()
+    process_controller = ProcessController()
+    process_controller.set_max_proc(3)
+    process_controller.start(
+        [
+            (get_test_task, (5,)),
+            (another_task, (5, 9, 2)),
+            (get_test_task, (3,)),
+            (another_task, (3, 0, 1)),
+        ],
+        4
+    )
+    time.sleep(1)
+    (process_controller.wait_count())
+    (process_controller.alive_count())
+    time.sleep(2)
+    (process_controller.wait_count())
+    (process_controller.alive_count())
+    process_controller.wait()
+
 
 
 if __name__ == "__main__":
